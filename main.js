9a2cafe6cb0ffe21ddd95b6ba4cbd79bc0b8fdb9
Daryl Wada <daryl.wada@gmail.com>
2018-06-06 11:02:59 -0700

    Changed the CSS class structure and styling of the cart summary page. #5

--- main.js
+++ main.js
@@ -89,7 +89,88 @@ var app = {
     item: null
   },
   cart: {
-    items: []
+    items: [
+      {
+        itemId: 1,
+        brand: 'Ludwig',
+        name: 'Black Beauty',
+        price: 749.99,
+        description: 'A snare drum for people with too many drumsticks.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'United States',
+        imageUrl: 'https://goo.gl/ubY0QA'
+      },
+      {
+        itemId: 2,
+        name: 'Darbuka',
+        brand: 'Vatan',
+        price: 79.99,
+        description: 'A hand drum for people who like belly dancing.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'Turkey',
+        imageUrl: 'https://goo.gl/Q7X6VB'
+      },
+      {
+        itemId: 3,
+        name: 'Djembe',
+        brand: 'Remo',
+        price: 49.99,
+        description: 'A hand drum for people with dreadlocks.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'West Africa',
+        imageUrl: 'https://goo.gl/iKtU8x'
+      },
+      {
+        itemId: 4,
+        name: 'Hi-Hats',
+        brand: 'Paiste',
+        price: 400,
+        description: 'A set of hats for people who spare no expense.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'Switzerland',
+        imageUrl: 'https://goo.gl/LV9l8O'
+      },
+      {
+        itemId: 5,
+        name: 'Giant Step',
+        brand: 'Sonor',
+        price: 599,
+        description: 'A pedal for metal drummers with only one foot.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'Germany',
+        imageUrl: 'https://goo.gl/zsFo1q'
+      },
+      {
+        itemId: 6,
+        name: 'Travis Barker Signature',
+        brand: 'Zildjian',
+        price: 24.99,
+        description: 'A set of drumsticks from the 90s.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'United States',
+        imageUrl: 'https://goo.gl/Cro3tL'
+      },
+      {
+        itemId: 7,
+        name: 'Bodhran',
+        brand: 'Meinl',
+        price: 129.99,
+        description: 'A frame drum for people who like to jig.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'Ireland',
+        imageUrl: 'https://goo.gl/YNb74O'
+      },
+      {
+        itemId: 8,
+        name: 'TR-09',
+        brand: 'Roland',
+        price: 399,
+        description: 'A drum machine for people with no desk space.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'Japan',
+        imageUrl: 'https://goo.gl/gJYUxz'
+      }
+    ]
   }
 }
 
@@ -164,15 +245,11 @@ function renderDetails(item) {
 }
 
 function renderCartItem(item) {
-  return createElement('div', { class: 'row' }, [
-    createElement('div', { class: 'col-4' }, [
-      createElement('img', { class: 'card-img-top', src: item.imageUrl, alt: 'Card image cap' }, [])
-    ]),
-    createElement('div', { class: 'col-8' }, [
-      createElement('h5', { class: 'card-title' }, [item.brand]),
-      createElement('p', { class: 'card-text item-name' }, [item.name]),
-      createElement('p', { class: 'badge' }, [currencyFormat(item.price)])
-    ])
+  return createElement('div', { class: 'col-6 offset-3 cart-col' }, [
+    createElement('img', { class: 'cart-img', src: item.imageUrl, alt: 'Card image cap' }, []),
+    createElement('h5', { class: 'card-title cart' }, [item.brand]),
+    createElement('p', { class: 'card-text item-name cart' }, [item.name]),
+    createElement('p', { class: 'card-text cart' }, [currencyFormat(item.price)])
   ])
 }
 
@@ -180,20 +257,20 @@ function renderCartTotal(cart) {
   var total = cart.items.reduce((acc, item) => {
     return acc + item.price
   }, 0)
-  return createElement('div', { class: 'row' }, [
-    createElement('span', { class: 'cart-total' }, ['Total: ' + total])
+  return createElement('div', { class: 'col-6 offset-3 ' }, [
+    createElement('div', { class: 'text-right cart-total' }, ['Total: ' + currencyFormat(total)])
   ])
 }
 
 function renderCartItemCount(cart) {
-  return createElement('div', { class: 'row' }, [
-    createElement('span', { class: 'cart-item-count' }, ['Items: ' + cart.items.length])
+  return createElement('div', { class: 'col-6 offset-3' }, [
+    createElement('div', { class: 'cart-item-count text-right cart-total' }, ['Items: ' + cart.items.length])
   ])
 }
 
 function renderCartSummary(cart) {
   var $summary = createElement('div', { class: 'container' }, [
-    createElement('h2', { class: 'cart-header' }, ['Cart Summary'])
+    createElement('h2', { class: 'cart-header text-center' }, ['Cart Summary'])
   ])
 
   app.cart.items.forEach((item) => {
